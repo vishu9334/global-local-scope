245b208a4610d88d4263741600b96c7419915640
vishu9334 <144935874+vishu9334@users.noreply.github.com>
2024-02-07 05:33:31 +0000

    Global & local scope

---
+++ index.js
@@ -0,0 +1,18 @@
+// var c =300
+let a = 300
+
+if (true){
+    let a =10
+    const b =20
+    // var c =30 var is not a good for variable in the program becouse
+    // var is not work proper in local $ global scope
+    console.log("INNER", a);
+    
+     
+}
+
+
+console.log(a);
+// console.log(b);
+// console.log(c);
+
